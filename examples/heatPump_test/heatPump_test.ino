b31551e48a8e47a614b8c94c3754a8049b4055a8
SwiCago <al@swicago.com>
2017-01-10 09:49:33 -0600

    Update heatPump_test.ino
    typo

--- examples/heatPump_test/heatPump_test.ino
+++ examples/heatPump_test/heatPump_test.ino
@@ -5,7 +5,7 @@ void setup() {
   // put your setup code here, to run once:
  HeatPump hp;
  hp.connect(&Serial);     //For ESP8266
- //hp.connect(&Serial1);  //Use UART1 or Arduino Mirco Pro
+ //hp.connect(&Serial1);  //Use UART1 or Arduino Micro Pro
  hp.update();  //   power mode  temp fan vane dir 
  String settings[6]={"ON","FAN","26","4","3","|"};
  hp.setSettings(settings);
