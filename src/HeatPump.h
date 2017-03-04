c79968bacbc167a18c8fa54d6e2bad5db21ecdc4
Adam Schroder <adamschroder@gmail.com>
2017-03-04 14:54:10 +1100

    Allow external update from IR

--- src/HeatPump.h
+++ src/HeatPump.h
@@ -149,7 +149,8 @@ class HeatPump
     bool connected = false;
     bool autoUpdate;
     bool firstRun;
-    bool tempMode; 
+    bool tempMode;
+    bool externalUpdate;
 
     String lookupByteMapValue(const String valuesMap[], const byte byteMap[], int len, byte byteValue);
     int    lookupByteMapValue(const int valuesMap[], const byte byteMap[], int len, byte byteValue);
@@ -182,6 +183,7 @@ class HeatPump
     bool connect(HardwareSerial *serial);
     bool update();
     void sync(byte packetType = PACKET_TYPE_DEFAULT);
+    void enableExternalUpdate();
     void enableAutoUpdate();
     void disableAutoUpdate();
 
