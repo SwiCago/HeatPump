f90e4642313f34611622b3eab07ec0540794f411
SwiCago <al@swicago.com>
2017-07-24 18:32:50 -0500

    Added note about PubSubClient.h
    Added note about PubSubClient.h and  MQTT_MAX_PACKET_SIZE

--- examples/mitsubishi_heatpump_mqtt_esp8266/mitsubishi_heatpump_mqtt_esp8266.h
+++ examples/mitsubishi_heatpump_mqtt_esp8266/mitsubishi_heatpump_mqtt_esp8266.h
@@ -12,6 +12,7 @@ const char* mqtt_username = "<YOUR MQTT USERNAME GOES HERE>";
 const char* mqtt_password = "<YOUR MQTT PASSWORD GOES HERE>";
 
 // mqtt client settings
+// Note PubSubClient.h has a MQTT_MAX_PACKET_SIZE of 128 defined, so either raise it to 256 or use short topics
 const char* client_id                   = "heatpump-controller-1"; // Must be unique on the MQTT network
 const char* heatpump_topic              = "heatpump";
 const char* heatpump_set_topic          = "heatpump/set";
